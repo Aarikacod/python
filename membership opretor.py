1570b119ac81d4783dea62898cb96ac794fc507c
Aarikacod <aarikaamukherjee@gmail.com>
2026-08-09 19:40:41 +0530

    initial commit

---
+++ membership opretor.py
@@ -0,0 +1,18 @@
+print("marks obtained")
+e = 80
+s = 70
+m = 90
+ss = 60
+c = 100
+avg = (80+70+90+60+100)/ 5
+
+
+if avg in range(90,100):
+    print("A1")
+elif avg in range (80,89):
+    print("A2")
+elif avg in range (70,79):
+    print("A3")
+elif avg in range (60,69):
+    print("B1")
+  
