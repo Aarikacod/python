a157af265e9ba63b016fb584aa585412c5aa54a1
Aarikacod <aarikaamukherjee@gmail.com>
2026-08-16 19:38:44 +0530

    initial commit

---
+++ mean value.py
@@ -0,0 +1,9 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_number = 48
+sum = mean1 * total_number
+print("the sum of number:", sum)
+number = sum-(wrong_number - correct_number)
+mean2 = number / total_number
+print("mean2", mean2)
