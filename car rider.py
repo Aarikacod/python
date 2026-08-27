cfc5df63b9be94d088e36e3797e2424617ff92e2
Aarikacod <aarikaamukherjee@gmail.com>
2026-08-27 18:38:16 +0530

    car rider

---
+++ car rider.py
@@ -0,0 +1,38 @@
+print ("===========================================================")
+print ("welcome to car rider")
+print ("===========================================================")
+
+("pick your vehicle")
+print ("1 bike")
+print("2 car")
+print()
+
+
+choice = int(input("Enter 1 or 2:"))
+print()
+
+
+if choice == 1:
+
+    print("step 2: Pick your bike type")
+    print("step 3: Pick your car type")
+
+
+    print("step 2: pick your bike type")
+    print("pick 1 - Scooty")
+    print("pick 2 - mountain")
+    bike_tupe = int(input("Enter your choice"))
+    if bike_type == 1:
+        print("Scooty")
+        print("Mountain bike")
+    else:
+        print("Mountain bike")
+elif choice == 2:
+    print("step 2:  Pick your car type")
+    print("pick 1 - Seden")
+    print("Enjoy your ride")
+    
+else:
+    choice = int(input("Enter 1 or 2:"))
+
+
