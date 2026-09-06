bd749d6b56673ecf3b0bbca1b5680a04f5191edb
Aarikacod <aarikaamukherjee@gmail.com>
2026-09-06 19:43:21 +0530

    initial commit

---
+++ checklist countdown.py
@@ -0,0 +1,32 @@
+Total_chores = 1 
+original_chores = 3
+
+
+print("you have original chores to finish")
+
+
+completed_chores = 2
+chore_num = 1
+
+
+while chore_num <= Total_chores:
+    print("chore_num and total_chores")
+
+
+    if chore_num == 1: "Make your bed"
+    elif chore_num == 2: "Feed your pet"
+    elif chore_num == 3: "Throw the trash"
+
+
+    Answer = input ("Enter your answer")
+    if Answer == "yes":
+        completed_count = 1
+        chore_num = 1 
+
+
+        print("Great job chore completed")
+
+
+    else:
+        print("if not completed with chore then complete")
+    
